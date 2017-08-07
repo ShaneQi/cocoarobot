b37167285766ebc0a766ac9995c06d753850c321
Shane Qi <qizengtai@gmail.com>
2017-08-07 13:50:56 -0500

    Added build script.

---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+docker run \
+-it \
+--rm \
+--name cocoarobot_build \
+-v `pwd`:/cocoarobot \
+-w /cocoarobot \
+swift:3.1.0 \
+/bin/bash -c \
+"\
+apt update && apt install libmysqlclient-dev -y;\
+swift build && ./.build/debug/cocoarobot;\
+"
